da64f5d20cacaff36b3be329299a075ba0c51788
abraaoribeiro <abraaohdg@gmail.com>
2021-10-30 13:54:00 -0300

    feat: Adiciona criteriaBuilder e Specification
    Foi implementado o querys utilizando o criteriaBuilder e
    Utilizando o padrão Specification.

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,9 +2,11 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante,Long>,
+        RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
 }
